87e6a6d7839646311d63cedaaaeaf0d0d9750ede
janosbuzasi <janos.buzasi@bluewin.ch>
2024-09-19 00:02:52 +0200

    Update about_us.php

--- about_us.php
+++ about_us.php
@@ -1,11 +1,14 @@
 <?php
+$title = 'Über uns';
 include 'header.php';
 ?>
 
 <!-- Body-Bereich -->
 <main>
     <h2>Über uns</h2>
-    <p>WEBTRASH.CH ist eine innovative Webdesign-Agentur, die sich auf kreative und maßgeschneiderte Lösungen spezialisiert hat. Wir arbeiten eng mit unseren Kunden zusammen, um ihre digitale Präsenz zu stärken.</p>
+    <p>
+        <?php echo $_SERVER['HTTP_HOST']; ?> ist eine innovative Webdesign-Agentur, die sich auf kreative und maßgeschneiderte Lösungen spezialisiert hat. Wir arbeiten eng mit unseren Kunden zusammen, um ihre digitale Präsenz zu stärken.
+    </p>
 </main>
 
 <?php
