594e6aecc32b08f967b3f9b6afd434dcb68d23f8
janosbuzasi <janos.buzasi@bluewin.ch>
2024-09-18 22:27:25 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -5,7 +5,7 @@ include 'header.php';
 <!-- Body-Bereich -->
 <main>
     <h2>Kontakt</h2>
-    <p>Haben Sie Fragen? Kontaktieren Sie uns per E-Mail unter <a href="mailto:info@webtrash.ch">info@webtrash.ch</a> oder rufen Sie uns an unter +41 44 123 45 67.</p>
+    <p>Haben Sie Fragen? Kontaktieren Sie uns per E-Mail oder rufen Sie uns an.</p>
 </main>
 
 <?php
