e5bf339d2157996bcbb15622c6dd82d1f4f00c5d
janosbuzasi <janos.buzasi@bluewin.ch>
2024-09-18 22:56:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ include 'header.php';
     <?php
     // Dynamische Begrüßung im Body
     echo '<p class="welcome-text">Willkommen auf unserer Seite!</p>';
-    echo '<p class="welcome-text">Welcome to our website!</p>';
     ?>
 </main>
 
