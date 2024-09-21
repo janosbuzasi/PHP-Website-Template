6216b04cbb5d898f912379ec9c17f14d8c0f1594
janosbuzasi <janos.buzasi@bluewin.ch>
2024-09-21 16:48:43 +0200

    Update about_us.php

--- about_us.php
+++ about_us.php
@@ -5,7 +5,7 @@ require 'header.php';  // Inkludiere den Header
 <!-- Restlicher Inhalt der Seite -->
 <!-- about_us Body-Bereich -->
 <main>
-    <h2>Über uns</h2>
+    <h2><?php $title ?></h2>
     <p>
         <?php echo $_SERVER['HTTP_HOST']; ?> ist eine innovative Webdesign-Agentur, die sich auf kreative und maßgeschneiderte Lösungen spezialisiert hat. Wir arbeiten eng mit unseren Kunden zusammen, um ihre digitale Präsenz zu stärken.
     </p>
